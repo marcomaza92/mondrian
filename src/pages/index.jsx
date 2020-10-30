1290b4425a6fbb9a2020899a9140296790c0ef6c
marcomaza92 <marcoagustinmaza@gmail.com>
2020-10-29 23:58:32 -0300

    feat(app): add pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Card from '../components/Card';
+import styles from './index.module.scss';
+
+const HomePage = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <Card />
+    </div>
+  )
+}
+
+export default HomePage;
